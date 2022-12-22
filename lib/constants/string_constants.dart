758a67f38d44a7f63e08c9766d37f73c7eb8bd6c
usuario <gomes.amilson@gmail.com>
2022-12-22 04:23:39 -0300

    criado arquivo string_constants.dart

---
+++ lib/constants/string_constants.dart
@@ -0,0 +1,32 @@
+class StringConstants {
+  static const nome = ' Nome';
+  static const sobreNome = ' SobreNome';
+  static const digiteNome = 'Digite o Nome';
+  static const digiteSobreNome = 'Digite o sobrenome';
+
+  static const email = 'Email:';
+  static const digiteEmail = 'Digite o email:';
+
+  static const login = ' Login';
+  static const senha = ' Senha';
+  static const digiteSenha = 'Digite a Senha';
+  static const repitaSenha = 'Confirme a Senha';
+  static const senhanaoconfere = 'Senhas nao Conferem!';
+
+
+  static const esqueceuSenha = 'Esqueceu a Senha?';
+  static const naoTemCadastro = 'Nao tem Cadastro ? ';
+  static const cadastrar = 'Cadastrar';
+
+  static const buscarUsuario = 'Buscar usuario';
+  static const jaecadastrado = 'Ja é Cadastrado?';
+  static const fazerLogin = 'FazerLogin';
+  static const loguinUserInfos = 'LOGIN_USER_INFOS';
+  static const clienteRegistrado = 'Cliente registrado com sucesso!';
+  static const desejaSair = 'Deseja sair?';
+  static const cancelar = ' Cancelar';
+  static const sair = 'Sair';
+  static const minhaConta = 'Minha Conta';
+  static const  configuracoes = 'Configuraçoes';
+  static const  aguarde = 'Aguarde';
+}
