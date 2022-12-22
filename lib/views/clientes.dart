e241f6194b064cce1aa367d6fd2e5a511b2319e3
amilson gomes <gomes.amilson@gmail.com>
2022-12-22 20:37:05 -0300

    atualizado com listview

--- lib/views/clientes.dart
+++ lib/views/clientes.dart
@@ -5,25 +5,31 @@ import '../service/get_user.dart';
 import 'cad_cli.dart';
 
 class ClientPage extends StatelessWidget {
-  const ClientPage({Key? key}) : super(key: key);
+
+UserModel userModel = UserModel();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Minha tela'),
+        title: const Text('Minha tela'),
       ),
-      body: ListView.builder(
-        itemCount: 1, // Set this to the number of items you want to display in the listview
-        itemBuilder: (context, index) {
-          // Get the saved user information
-          UserModel? userModel = LoginUser().getsavedUser(context) as UserModel?;
-
-          // Return a list tile with the user information
-          return ListTile(
-            title: Text(userModel!.name.toString()),
-            subtitle: Text(userModel.mail.toString()),
-          );
+      body: FutureBuilder(
+        future: LoginUser().getsavedUser(context),
+        builder: (BuildContext context, AsyncSnapshot<UserModel?> snapshot) {
+          if (snapshot.hasData) {
+            UserModel? user = snapshot.data;
+            return ListView(
+              children: [
+                Text(user!.name.toString()),
+                Text(user.mail.toString()),
+                Text(user.phone.toString()),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          return const CircularProgressIndicator();
         },
       ),
 
