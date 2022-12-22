43395abcc333d25517a37dd8feafca4e8dc09e51
usuario <gomes.amilson@gmail.com>
2022-12-22 04:22:45 -0300

    criado arquivo error_constants.dart

---
+++ lib/constants/error_constants.dart
@@ -0,0 +1,6 @@
+class ErrorConstants{
+  static const ApiErrorLogin = 'usuario ou senha invalidos';
+  static const ApiError = 'Erro ao acessar a Pagina';
+  static const errorRegister = 'erro ao Registrar';
+  static const userNotRegister = 'Usuario nao cadastrado';
+}
