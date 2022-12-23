6b2ddd4de4f6e58aff79d1be914fd7e345ccccfe
amilson gomes <gomes.amilson@gmail.com>
2022-12-23 13:22:36 -0300

    atualizado função

--- lib/service/get_user.dart
+++ lib/service/get_user.dart
@@ -1,20 +1,21 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../constants/error_constants.dart';
 import '../constants/string_constants.dart';
 import '../model/client_model.dart';
 
 class LoginUser {
-
-  Future<UserModel?> getsavedUser(context) async {
+  List<dynamic> list = [];
+  Future<List?> getsavedUser(context) async {
     try {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       String? jsonUser = prefs.getString(StringConstants.loguinUserInfos);
       Map<String, dynamic> mapUser = json.decode(jsonUser.toString());
       UserModel userModel = UserModel.fromJson(mapUser);
-      return userModel;
+      list= [userModel.name,userModel.mail,userModel.phone].toList();
+
+
+      return list;
     } catch (e) {
       print('${ErrorConstants.userNotRegister}$e');
     }
